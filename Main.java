a24034d7f455a3242ddc3dec7a3107ad442343e9
Christoph Gardner <78000789+christoph-gardner@users.noreply.github.com>
2022-06-02 15:19:23 +0000

    ask for name

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.util.*;
+import java.io.*;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scn = new Scanner(System.in);
+    introduction(scn);
+
+    int boardLength = 3;
+    int boardHeight = 3;
+    
+    
+  }
+
+  public static void introduction(Scanner scn){
+    System.out.println("Welcome to special tic-tac-toe!");
+    System.out.println("What's your name? ");
+    String name = scn.next();
+    System.out.println("Welcome, " + name + "!");
   }
 }
